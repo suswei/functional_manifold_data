d43cd2e9e0a301675fad258a0735df32c6f37bd2
Susan Wei <stawbygl@gmail.com>
2018-11-01 13:38:39 +1100

    did almost nothing, just moved commented area around

--- main.R
+++ main.R
@@ -24,19 +24,7 @@ scms_h = 1 #bandwidth para.
 dim_mani <- 1
 num_neigh <- 10
 
-# manifold estimation via local covariance flow
-# x0 = as.numeric(obj$data[500,])
-# sigma = 1
-# kernel = scms$make_isotropic_gaussian_kernel(sigma)
-# ms_iterations = 1000
-# for (j in 1:ms_iterations){
-#   x0shifted = x0 + scms$mean_shift_update(x0, data, kernel)
-#   x0 = x0shifted
-# }
-# shift_type = "ms"
-# kernel = "double-peak"
-# eig_h = 0.9
-# path = eigenvectorFlow(obj, x0shifted, shift_type, kernel, eig_h, FALSE)
+
 
 
 
@@ -60,9 +48,6 @@ Iso_true <- Isomap(true_mani, dim_mani, num_neigh)
 Iso_obs_data <- Isomap(data, dim_mani, num_neigh)
 Iso_denoised_data <- Isomap(denoised, dim_mani, num_neigh)
 
-# standardize geodesic distance matrices
-
-
 rel_err_obs_data <- sum((Iso_true[[2]]-Iso_obs_data[[2]])^2)/sum(Iso_true[[2]]^2)
 rel_err_denoised_data <- sum((Iso_true[[2]]-Iso_denoised_data[[2]])^2)/sum(Iso_true[[2]]^2)
 
@@ -85,4 +70,20 @@ if(dim_mani==2){
 	Iso_comp_den <- Iso_denoised_data[[1]][[1]]
 	plot(1:samplesize,Iso_comp_den[,1],main='mani comp denoi.')
 }
+
+
+# TODO: how to project onto
+# manifold estimation via local covariance flow
+# x0 = as.numeric(obj$data[500,])
+# sigma = 1
+# kernel = scms$make_isotropic_gaussian_kernel(sigma)
+# ms_iterations = 1000
+# for (j in 1:ms_iterations){
+#   x0shifted = x0 + scms$mean_shift_update(x0, data, kernel)
+#   x0 = x0shifted
+# }
+# shift_type = "ms"
+# kernel = "double-peak"
+# eig_h = 0.9
+# path = eigenvectorFlow(obj, x0shifted, shift_type, kernel, eig_h, FALSE)
 
