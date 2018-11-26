78350efcabe68724d1330254aa6210d472c71310
Susan Wei <stawbygl@gmail.com>
2018-11-26 17:45:13 +1100

    comparing MDS with individual and aggregate (median) RP

--- MDSversusRP.py
+++ MDSversusRP.py
@@ -7,15 +7,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # load data
-X, color = datasets.samples_generator.make_swiss_roll(n_samples=1500)
+n = 1500
+X, color = datasets.samples_generator.make_swiss_roll(n_samples=n)
 dg_true = gaig.get_auto_isomap_gdist(X)
 
-
+# Random projection to reduce dimension, then shift to density ridge
 P = 1000
-RP_error = np.empty((0, P))
+RP_error = []
+dg_RP_matrix = np.empty((0, n**2))
 for i in range(P):
     # embed into lower dimension using RP
-    n_components =2
+    n_components = 2
     transformer = random_projection.GaussianRandomProjection(n_components=n_components)
     X_transformed_RP = transformer.fit_transform(X)
     # compute density ridge
@@ -23,9 +25,16 @@ for i in range(P):
     projected_RP = scms.scms(X_transformed_RP, sigma, n_iterations=5)
     # geodesic distance
     dg_RP = gaig.get_auto_isomap_gdist(projected_RP)
+    dg_RP_matrix = np.vstack((dg_RP_matrix, dg_RP.flatten()))
     # calculate error
     diff = dg_RP - dg_true
     RP_error = np.append(RP_error, (diff ** 2).mean())
+
+# calculate elementwise median for dg_RPs
+dg_RP_median = np.median(dg_RP_matrix,axis=0)
+# calculate errors
+diff = dg_RP_median - dg_true.flatten()
+RP_median_error = (diff ** 2).mean()
 
 
 # embed into lower dimensions using MDS
@@ -41,6 +50,9 @@ dg_MDS = gaig.get_auto_isomap_gdist(projected_MDS)
 diff = dg_MDS - dg_true
 MDS_error = (diff ** 2).mean()
 
-# plot histogram of RP errors with MDS as verticle line
-plt.hist(RP_error, bins=20, color='c', edgecolor='k', alpha=0.65)
-plt.axvline(MDS_error, color='k', linestyle='dashed', linewidth=1)
+# plot histogram of RP errors with MDS and median RP as references
+plt.hist(RP_error, bins=20, color='c', edgecolor='k', alpha=0.65,label='individual RP error')
+plt.axvline(MDS_error, color='k', linestyle='dashed', linewidth=1,label='MDS error')
+plt.axvline(RP_median_error, color='r', linestyle='dashed', linewidth=1,label='median RP error')
+plt.legend()
+plt.show()
