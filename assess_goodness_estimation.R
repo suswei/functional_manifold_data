04a9ead7067244c29acd7bac4f7a3ae37317b65b
Susan Wei <stawbygl@gmail.com>
2019-05-09 18:33:36 +1000

    cleaned up file dependencies and wrote a notebook for testing whether analytic calculations are correct

--- assess_goodness_estimation.R
+++ assess_goodness_estimation.R
@@ -1,5 +1,7 @@
 ##### Function that assess how good a particular estimation of a geodesic matrix is good
 
+library(MESS) # for auc function
+
 # for the moment we just use the relative error || hat A - A ||_F / || A ||_F
 
 assess_goodness_estimation <- function(estim_mat,true_geo){
--- sim_functional_data.R
+++ sim_functional_data.R
@@ -1,8 +1,8 @@
 ##### Dataset generators
 
-# Scenario 1 : random function from the manifolg M={X(t)=u(h_alpha(t)},with h_alpha(t)=t-alpha and u(t)=t
-# TODO Marie: the true geodesic distance calculation is incorrect for scenario 2
-# Scenario 2 : random function from the manifolg M={X(t)=density N(alpha,1)}
+# Scenario 1 : random function from the manifold M={X(t)=u(h_alpha(t)},with h_alpha(t)=t-alpha and u(t)=t
+# TODO Marie: the analytic geodesic distance calculation is incorrect for scenario 2
+# Scenario 2 : random function from the manifold M={X(t)=density N(alpha,1)}
 
 # TODO Marie/Susan: we need more functional scenarios!
 
@@ -13,19 +13,22 @@
 # reg_sampling : if 0 points on the manifold are draw from a norm dist. and if 1 regular sampling 
 # a,b : functions X(t) are defined for t in [a,b]
 # com_grid : if 1 each curve is observed on the same grid and if 0 the grid for each curve is randomly genarated from unif[a,b]
-# plot_true : if 1 plot the true data, the observed the data and the true geodesic matrix
+# plot_true : if 1 plot the true data, the observed data and the true geodesic matrix
 
 ## Output
-# true_data : samplesize x K matrix containing the original data (no noise)
-# discrete_data : samplesize x K matrix containing the observed data
-# true_geo : samplesize x samplesize matrix containing true pairwise geo disctance
+# noiseless_data : samplesize x K matrix containing the original data (no noise)
+# noisy_data : samplesize x K matrix containing the observed data
+# analytic_geo : samplesize x samplesize matrix containing analytic pairwise geo disctance
 # grid : samplesize x K matrix containing the grid on which each data is observed
 # reg_grid : vector of dim K containing a common grid to use for smoothing
 
 #Example of call
 # data<- sim_functional_data(1,20,15,-3,3,0.5,1,1,1)
 
-sim_functional_data<-function(sce,samplesize,K,a,b,SNR,reg_sampling,com_grid,plot_true){
+library(fields)
+source('full_geo_from_adj_geo.R')
+
+sim_functional_data<-function(sce,samplesize=100,K=20,a=0,b=1,SNR=1,reg_sampling=1,com_grid=1,plot_true=1){
   
   if(sce == 1){
     if(reg_sampling==0){
@@ -40,9 +43,9 @@ sim_functional_data<-function(sce,samplesize,K,a,b,SNR,reg_sampling,com_grid,plo
       h_t <- t-al
     }
     
-    ### Calculate the true geodesic matrix
     adja_geo <- (alpha[-1]- alpha[-samplesize])*sqrt(b-a)
-    true_geo <- full_geo(adja_geo,samplesize)
+    ### Calculate the analytic geodesic matrix
+    analytic_geo <- full_geo(adja_geo,samplesize)
     
   } else if(sce ==2){
     
@@ -58,44 +61,44 @@ sim_functional_data<-function(sce,samplesize,K,a,b,SNR,reg_sampling,com_grid,plo
       fct <- dnorm(t,al,1)
     }
     
-    ### Calculate the true geodesic matrix
     adja_geo <- (alpha[-1]- alpha[-samplesize])/(2*pi^(1/4))
-    true_geo <- full_geo(adja_geo,samplesize)
+    ### Calculate the analytic geodesic matrix
+    analytic_geo <- full_geo(adja_geo,samplesize)
     
   }
   
-  true_data <- matrix(ncol=K,nrow=samplesize)
+  noiseless_data <- matrix(ncol=K,nrow=samplesize)
   grid<-matrix(ncol=K,nrow=samplesize)
   reg_grid=seq(a,b,length.out=K)
   if(com_grid==0){
-    reg_true_data = matrix(ncol=K,nrow=samplesize)
+    reg_noiseless_data = matrix(ncol=K,nrow=samplesize)
     for(i in 1:samplesize){
       tmp_grid=sort(runif(K,a,b))
       grid[i,]=tmp_grid
-      true_data[i,] <- mu_t(tmp_grid,alpha[i])
-      reg_true_data[i,]<-mu_t(reg_grid,alpha[i])
+      noiseless_data[i,] <- mu_t(tmp_grid,alpha[i])
+      reg_noiseless_data[i,]<-mu_t(reg_grid,alpha[i])
     }
   }else if (com_grid==1){
     for(i in 1:samplesize){
       grid[i,]=reg_grid
-      true_data[i,] <- mu_t(reg_grid,alpha[i])
+      noiseless_data[i,] <- mu_t(reg_grid,alpha[i])
     }
-    reg_true_data=true_data
+    reg_noiseless_data=noiseless_data
   }
     
-  mean_signal= apply(reg_true_data,2,mean)
-  var_signal= (1/(samplesize*K))*sum((reg_true_data-matrix(mean_signal,ncol=K,nrow=samplesize,byrow=TRUE))^2)
+  mean_signal= apply(reg_noiseless_data,2,mean)
+  var_signal= (1/(samplesize*K))*sum((reg_noiseless_data-matrix(mean_signal,ncol=K,nrow=samplesize,byrow=TRUE))^2)
   sd_noise= sqrt(var_signal/(10^(SNR/10)))
     
   epsilon<- matrix(rnorm(samplesize*K,0,sd_noise),nrow=samplesize)
-  discrete_data <- (true_data + epsilon)
+  noisy_data <- (noiseless_data + epsilon)
     
   if (plot_true==1){
     par(mfrow=c(1,3))
-    matplot(t(grid),t(true_data),main="True data",type='l', col=rainbow(samplesize))
-    matplot(t(grid),t(discrete_data),main="Observed data",type='l', col=rainbow(samplesize))
-    image.plot(true_geo,main='True geodesic')
+    matplot(t(grid),t(noiseless_data),main="True data",type='l', col=rainbow(samplesize))
+    matplot(t(grid),t(noisy_data),main="Observed data",type='l', col=rainbow(samplesize))
+    image.plot(analytic_geo,main='analytic geodesic')
   }
     
-  return(list('true_data'=true_data,'discrete_data'=discrete_data,'true_geo'=true_geo,'grid'=grid,'reg_grid'=reg_grid))
+  return(list('noiseless_data'=noiseless_data,'noisy_data'=noisy_data,'analytic_geo'=analytic_geo,'grid'=grid,'reg_grid'=reg_grid))
 }
