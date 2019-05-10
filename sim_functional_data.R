3f6a70f8f9841ed5b45e8b93ffbb697b641c5073
Susan Wei <stawbygl@gmail.com>
2019-05-10 16:29:00 +1000

    added significant math documentation to sim_functional_data function. added markdown doc for checking if analytic geodesic distance calculation is correct

--- sim_functional_data.R
+++ sim_functional_data.R
@@ -1,10 +1,15 @@
-##### Dataset generators
+#' ## This function generates functional data living in a submanifold of a Hilbert space
 
-# Scenario 1 : random function from the manifold M={X(t)=u(h_alpha(t)},with h_alpha(t)=t-alpha and u(t)=t
-# TODO Marie: the analytic geodesic distance calculation is incorrect for scenario 2
-# Scenario 2 : random function from the manifold M={X(t)=density N(alpha,1)}
-
-# TODO Marie/Susan: we need more functional scenarios!
+#' Scenario 1: Considered is the manifold $$M=\{X(t) = t-\alpha, -0.9 \le \alpha \le 3\} \subset L^2([a,b])$$ with inherited metric from the ambient $L^2[a,b]$ space.
+#' The shortest path $\gamma: [0,1] \to M$ between two functions $X_1 = (t-\alpha_1)$ and $X_2 = (t-\alpha_2)$ in $M$ is clearly given by $\gamma(t) = t X_1 + (1-t) X_2$.
+#' The geodesic distance between $X_1$ and $X_2$ is $$L(\gamma) := \int_0^1 || \dot \gamma(t) ||_{L^2} \,dt = ||X_1 - X_2||_{L_2} = (\alpha_1-\alpha_2)\sqrt{b-a}.$$ 
+#' Scenario 2 : Considered is the manifold $$ M = \{ \text{probability density function} N(\alpha,\sigma^2): \alpha \in \mathbb R, \sigma > 0 \} \subset L^2([-\infty,\infty])$$ with inherited metric from the ambient $L^2[-\infty,\infty]$ space. 
+#' Let $X_1$ be the pdf of $N(\alpha_1,\sigma_1^2)$ and $X_2$ be the pdf of $N(\alpha_2,\sigma_2^2)$. 
+#' Again, since $$ b\gamma(t) := t X_1 + (1-t) X_2$$ belongs to $M$ for all $t$, it must be the shortest path between $X_1$ and $X_2$. The geodesic distance between $X_1$ and $X_2$ is
+#' $$\int_0^1 || \dot \gamma(t) ||_{L^2} \,dt = ||X_1 - X_2||_{L_2}$$ which has no easy analytic solution.
+#' Marie's previous example where $$ M = \{ \text{probability density function } N(\alpha,1): \alpha \in \mathbb R \} $$ 
+#' is actually much harder to work with because the "straight" line connecting $X_1$ and $X_2$ in $M$ does not always stay inside of $M$. 
+#' To see this, note that $t N(\alpha_1,1) + (1-t) N(\alpha_2,1)$ no longer has variance $1$ for all $t \in [0,1]$. 
 
 ## Input 
 # samplesize
@@ -21,6 +26,8 @@
 # analytic_geo : samplesize x samplesize matrix containing analytic pairwise geo disctance
 # grid : samplesize x K matrix containing the grid on which each data is observed
 # reg_grid : vector of dim K containing a common grid to use for smoothing
+
+
 
 #Example of call
 # data<- sim_functional_data(1,20,15,-3,3,0.5,1,1,1)
@@ -43,6 +50,7 @@ sim_functional_data<-function(sce,samplesize=100,K=20,a=0,b=1,SNR=1,reg_sampling
       h_t <- t-al
     }
     
+    # TODO: Add mathematical derviation for adja_geo
     adja_geo <- (alpha[-1]- alpha[-samplesize])*sqrt(b-a)
     ### Calculate the analytic geodesic matrix
     analytic_geo <- full_geo(adja_geo,samplesize)
