53321e987c82d6c0191b763d8d190cf0421a41c5
Susan Wei <stawbygl@gmail.com>
2019-06-24 16:39:10 +0200

    added (3!) versions of tecator dataset

--- processRealData.R
+++ processRealData.R
@@ -1,4 +1,8 @@
-library("rlist")
+library(fda) # for growth dataset
+library("fda.usc") # for tecator dataset
+
+# returns a list of lists where every element describes a different dataset. all datasets are smoothed
+
 processRealData = function(){
   
   list_of_lists = list()
@@ -41,6 +45,55 @@ processRealData = function(){
     list_of_lists[[list_index+2]]= accmhat_list
     list_index = list_index + 3
   }
+  detach(growth)
+  
+  ### Tecator from fda package
+  # This is from Mahalanobis technometrics paper
+  # "The classification problem here is to separate meat samples with a high fat content(more than 20%) from samples with a low fat content (less than20%) based on absorbance. 
+  # Among the 215 samples, 77 have high fat content and 138 have low fat content."
+  
+  
+  # Warning: there are some weird boundary effects here!
+  # Mahalanobis paper regarding smoothing
+  # "second-order derivatives of the observed functions produces lower misclassification rates. 
+  # Therefore, the analysis of the original data and their second-order derivatives are carried out. 
+  # In both cases, the original discrete observations and their second differences are converted to 
+  # functional observations using a B- spline basis of order 6 with 20 and 40 basis functions, respectively."
+  
+  data(tecator)
+  absorp <- tecator$absorp.fdata
+  Fat20 <- ifelse(tecator$y$Fat < 20, 0, 1) # class membership
+  
+  # bspline parameters
+  norder <- 6
+  lambda <- 1e-2
+  
+  
+  for (nbasis in c(20,40)) {
+    basis <- create.bspline.basis(tecator[["absorp.fdata"]]$rangeval, nbasis, norder)
+    growfdPar <- fdPar(basis, 0, lambda)
+    
+    tecator_fd <- smooth.basis(tecator[["absorp.fdata"]]$argvals, t(absorp$data), growfdPar)$fd
+    grid = absorp$argvals
+    # grid_chopped = grid[-c(1,length(grid))] #TODO: is this cheating? we have to do it to avoid boundary effects
+    if (nbasis==20){
+      # Evaluate on original time grid
+      list_of_lists[[list_index]] = list(data = eval.fd(grid, tecator_fd), reg_grid = grid, true_group = Fat20, name = "tecator")
+      list_index = list_index+1
+    } else{
+      # Evaluate on original time grid
+      list_of_lists[[list_index]] = list(data = eval.fd(grid, tecator_fd, 2), reg_grid = grid, true_group = Fat20, name = "tecator_der2")
+      list_index = list_index+1
+    }
+  }
+  
+  # Lin and Yao's contamination paper regarding tecator data preporcessing:
+  # we predict the fat content based on the first derivative curves approximated 
+  # by the difference quotient between measurements at adjacent wavelengths, shown in the left panel of Figure 2.
+  tecatorLY_fd=fdata.deriv(absorp,nderiv=1,method="diff",class.out='fdata')
+  list_of_lists[[list_index]] = list(data = tecatorLY_fd$data, reg_grid = tecatorLY_fd$argvals, true_group = Fat20, name = "tecator_LYdiff1storder")
+  list_index = list_index+1
   
   return(list_of_lists)
+
 }
